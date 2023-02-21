6afdf8cbb478d84934c2848560924346df376441
Eniekpo <johneniekpo4life@gmail.com>
2023-02-21 08:45:15 +0100

    a

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-        JSON.stringify({
-            data: "Hello, world",
-        })
-    );
-
+    if (req.url === '/') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.writable('pages/home.html');
+        res.end();
+    }
+    if (req.url === '/about') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.writable('<h1>I am About Page</h1>')
+        res.end();
+    } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("<h1>Page Not Found</h1>");
+        res.end();
+    }
 });
 console.log(`Server is Running at http://localhost:${PORT}`)
 server.listen(PORT);
