42a93a959d4f03061a1c63c72965bbf946cc8ad8
Eniekpo <johneniekpo4life@gmail.com>
2023-02-21 08:51:18 +0100

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.writable('pages/home.html');
+        res.write('pages/home.html');
         res.end();
     }
     if (req.url === '/about') {
