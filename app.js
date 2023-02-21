1623cebe3516efa2953d9a7c88c7f93a36b70484
Eniekpo <johneniekpo4life@gmail.com>
2023-02-21 01:02:58 +0100

    m

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-for (let index = 0; index < 10; index++) {
-    console.log(index);
-}
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+        JSON.stringify({
+            data: "Hello, world",
+        })
+    );
+
+});
+console.log(`Server is Running at http://localhost:${PORT}`)
+server.listen(PORT);
